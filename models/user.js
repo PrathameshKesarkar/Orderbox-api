6dd62cc9a257be1e03fdabc38951a732fa535365
Prathamesh Kesarkar <pratham.kesarkar@gmail.com>
2016-06-22 06:15:11 -0700

    Hashed and Salted Password

--- models/user.js
+++ models/user.js
@@ -41,6 +41,12 @@ module.exports = function (sequelize, DataTypes) {
                    user.email= user.email.toLowerCase();
                 }
             }
+        },
+        instanceMethods:{
+            toPublicJSON:function(){
+                var json = this.toJSON();
+                return _.pick(json,'id','email','updatedAt','createdAt')
+            }
         }
     });
 };
