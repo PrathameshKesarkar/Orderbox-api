de15985464ed3ba8364d4351bd445891c6fed241
Prathamesh Kesarkar <pratham.kesarkar@gmail.com>
2016-06-23 00:46:38 -0700

    Smiplifed login request and authenticate class method

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -13,40 +13,63 @@ module.exports = function (sequelize, DataTypes) {
                 isEmail: true
             }
         },
-        salt:{
-            type:DataTypes.STRING
+        salt: {
+            type: DataTypes.STRING
         },
-        password_hash:{
-            type:DataTypes.STRING
+        password_hash: {
+            type: DataTypes.STRING
         },
         password: {
             type: DataTypes.VIRTUAL,
             allowNull: false,
-            validate:{
-                len:[7,100]
+            validate: {
+                len: [7, 100]
             },
-            set:function (value) {
-                var salt=bcrypt.genSaltSync(10);
-                var hashedPassword=bcrypt.hashSync(value,salt);
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
 
-                this.setDataValue('password',value);
-                this.setDataValue('salt',salt);
-                this.setDataValue('password_hash',hashedPassword);
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
-    },{
-        hooks:{
-            beforeValidate:function (user, options) {
-                if(typeof user.email==='string'){
-                   user.email= user.email.toLowerCase();
+    }, {
+        hooks: {
+            beforeValidate: function (user, options) {
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
                 }
             }
         },
-        instanceMethods:{
-            toPublicJSON:function(){
+        classMethods: {
+            authenticate: function (body) {
+                return new Promise(function (resolve, reject) {
+                    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+                        return reject();
+                    }
+                    user.findOne({
+                        where: {
+                            email: body.email
+                        }
+                    }).then(function (user) {
+                        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                            return reject();
+                        }
+                        resolve(user);
+                    }, function (error) {
+                        reject();
+                    });
+                })
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function () {
                 var json = this.toJSON();
-                return _.pick(json,'id','email','updatedAt','createdAt')
+                return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt')
             }
         }
     });
+
+    return user;
 };
--- server.js
+++ server.js
@@ -5,30 +5,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db');
-
+var bcrypt = require('bcryptjs');
 
 var app = express();
-var PORT =process.env.PORT||3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.get('/',function (req,res) {
-   res.send('Order Box API Root');
+app.get('/', function (req, res) {
+    res.send('Order Box API Root');
 });
 
-app.post('/users',function (req, res) {
-    var body = _.pick(req.body,'email','password');
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function (user) {
         res.json(user.toPublicJSON());
-    },function (error) {
+    }, function (error) {
         res.status(400).json(error);
     });
 });
 
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    },function (error) {
+        res.status(401).send('Password or email maybe wrong');
+    });
+
+
+});
+
 db.sequelize.sync({force:true}).then(function () {
-    app.listen(PORT,function () {
-        console.log("Express is listening on Port "+PORT+"!");
-    });    
+    app.listen(PORT, function () {
+        console.log("Express is listening on Port " + PORT + "!");
+    });
 });
 
