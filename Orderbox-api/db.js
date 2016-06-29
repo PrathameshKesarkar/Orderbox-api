6ceac5c1e3234c57cec6afe4173cc435fd037ad3
Prathamesh Kesarkar <pratham.kesarkar@gmail.com>
2016-06-29 03:06:04 -0700

    pre-add JWT Token

---
+++ Orderbox-api/db.js
@@ -0,0 +1,24 @@
+/**
+ * Created by PrathamK on 6/21/2016.
+ */
+var Sequelize= require('sequelize');
+var env = process.env.NODE_ENV|| 'development';
+var sequelize;
+if(env==='production') {
+     sequelize = new Sequelize(process.env.DATABASE_URL,{
+         'dialect': 'postgres'
+     })
+}
+else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-order-box-api.sqlite'
+    });
+}
+var db ={};
+db.user=sequelize.import(__dirname+'/models/user.js');
+
+db.sequelize=sequelize;
+db.Sequelize=Sequelize;
+
+module.exports=db;
---
+++ Orderbox-api/server.js
@@ -0,0 +1,46 @@
+/**
+ * Created by PrathamK on 6/21/2016.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var db = require('./db');
+var bcrypt = require('bcryptjs');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.send('Order Box API Root');
+});
+
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function (error) {
+        res.status(400).json(error);
+    });
+});
+
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    },function (error) {
+        res.status(401).send('Password or email maybe wrong');
+    });
+
+
+});
+
+db.sequelize.sync({force:true}).then(function () {
+    app.listen(PORT, function () {
+        console.log("Express is listening on Port " + PORT + "!");
+    });
+});
+
