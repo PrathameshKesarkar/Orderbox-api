d5da996ebd79325d3f43decbddff10f8736d27de
Prathamesh Kesarkar <pratham.kesarkar@gmail.com>
2016-06-30 08:26:09 -0700

    Added the functionality to push the user local database to server directly

--- Orderbox-api/server.js
+++ Orderbox-api/server.js
@@ -6,9 +6,31 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db');
 var bcrypt = require('bcryptjs');
-
+var middleware = require('./middleware');
+var mkdirp = require('mkdirp');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var multer = require('multer');
+var fs = require('fs');
+var basePath = './public/databases';
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null,'./public/databases');
+    },
+    filename: function (req, file, cb) {
+
+        var getFileExt = function (filename) {
+            var fileExt= filename.split('.');
+            if(fileExt.length===1||fileExt[0]===''){
+                return"";
+            }
+            return fileExt.pop();
+        };
+
+        cb(null,Date.now()+'.'+getFileExt(file.originalname));
+    }
+});
+
 
 app.use(bodyParser.json());
 
@@ -20,11 +42,22 @@ app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function (user) {
+        var dir = basePath + '/' + user.id.toString();
+        mkdirp(dir, function (error) {
+            if (error) {
+                console.error(error);
+            }
+            else {
+                console.log('pow');
+            }
+        });
         res.json(user.toPublicJSON());
     }, function (error) {
         res.status(400).json(error);
     });
 });
+
+
 
 app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
@@ -39,7 +72,19 @@ app.post('/users/login', function (req, res) {
         res.status(401).send('Password or email maybe wrong');
     });
 
+});
 
+
+app.post('/database', multer({storage:storage}).single('upl'), function (req, res) {
+    console.log(req.file);
+    res.status(204).send();
+});
+
+app.get('/database',function (req, res) {
+    //res.send(path.join(__dirname,'/public/database','1467287165514.png'));
+    var file = fs.readFileSync('./public/databases/1467287165514.png');
+    res.writeHead(200,{'Content-Type':'image/png'});
+    res.end(file);
 });
 
 db.sequelize.sync({force: true}).then(function () {
